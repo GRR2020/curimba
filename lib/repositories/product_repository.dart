c569fe4b9c570f77f208223b9659d31ae9065d4a
jimaeda <reki.imaeda@gmail.com>
2020-12-14 19:30:46 -0300

    [Refactor] Rename field

--- lib/repositories/product_repository.dart
+++ lib/repositories/product_repository.dart
@@ -9,19 +9,20 @@ class ProductRepository {
 
   Future<List<ProductModel>> getFromUser(int userId) async {
     Database db = await DatabaseHelper.instance.database;
-    var dbCards = await db.query(
+    var productsByUserId = await db.query(
       table,
       where: 'users_id = ?',
       whereArgs: [userId],
     );
 
-    return List.generate(dbCards.length, (i) {
+    return List.generate(productsByUserId.length, (i) {
       return ProductModel(
-        id: dbCards[i]['id'],
-        usersId:  dbCards[i]['users_id'],
-        name: dbCards[i]['name'],
-        price:  dbCards[i]['price'],
-        description:  dbCards[i]['description'],
+        id: productsByUserId[i]['id'],
+        usersId: productsByUserId[i]['users_id'],
+        name: productsByUserId[i]['name'],
+        price: productsByUserId[i]['price'],
+        description: productsByUserId[i]['description'],
+        month: productsByUserId[i]['month'],
       );
     });
   }
