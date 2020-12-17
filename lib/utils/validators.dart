2577fb8609e40f77270b6ab0f9ad25146a16c91e
LucasPCBranco <lucas.pcbranco@hotmail.com>
2020-12-17 20:09:08 -0300

    [Refactor] Replace Error Code with Exception

--- lib/utils/validators.dart
+++ lib/utils/validators.dart
@@ -1,39 +1,41 @@
 import 'masks.dart';
+import 'value_exception.dart';
 
 class Validators {
   static validateBySize(String value, int size) {
     if (value.isEmpty || value.length < size) {
-      return "Campo deve ter no mínimo $size caracteres";
+      throw new ValueException("Campo deve ter no mínimo $size caracteres");
     }
     return null;
   }
 
   static validateNotEmpty(value) {
     if (value.isEmpty) {
-      return 'Complete o campo';
+      throw new ValueException("Complete o campo");
     }
     return null;
   }
 
   static validateDay(value) {
     if (value.isEmpty) {
-      return 'Complete o campo';
+      thrpe
     } else {
       var day = int.parse(value);
       if (day > 31 || day < 1) {
-        return 'Dia inválido';
+        throw new ValueException('Dia inválido')O;
       }
     }
     return null;
   }
 
   static validateMonth(value) {
-    if (value.isEmpty) {
-      return 'Complete o campo';
+    var notEmpty = validateNotEmpty(value);
+    if (notEmpty != null) {
+      return notEmpty;
     } else {
       var month = int.parse(value);
       if (month > 12 || month < 1) {
-        return 'Mês inválido';
+        throw new ValueException('Mês inválido');
       }
     }
     return null;
---
+++ lib/utils/value_exception.dart
@@ -0,0 +1,9 @@
+class ValueException implements Exception {
+    
+    private String _message;
+    
+    ValueException([String message = 'Um erro ocorreu ao criar esse campo']){
+        return message;
+    }
+    
+}
